b3694c73d045fb2226e9be198cc6c3d3905af87e
Guybrush <andrescolodrero@gmail.com>
2016-08-31 19:33:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Copyright IBM Corp. 2016. All Rights Reserved.
-// Node module: loopback-connector-db2
-// This file is licensed under the Artistic License 2.0.
-// License text available at https://opensource.org/licenses/Artistic-2.0
+
 
 module.exports = require('./lib/db2i.js');
